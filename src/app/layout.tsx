72e1be32e5ec4713806a692aa7b187d848f572d3
LinCie <aldiofernanda@protonmail.com>
2024-11-03 17:45:28 +0700

    remove mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "../theme";
-
 import "./globals.css";
-import DrawerAppBar from "@/components/layout/appbar";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -29,15 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className}`}>
-        <CssBaseline />
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <DrawerAppBar />
-            {children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
-      </body>
+      <body className={`${roboto.className}`}>{children}</body>
     </html>
   );
 }
